1b43e51364e3582a0a9f72067c3047038ec17d97
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-01-16 16:47:36 +0100

    pm2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nombre-de-tu-app',
+      script: 'dist/assets/index-BTokui3m.js',
+      watch: true,
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
